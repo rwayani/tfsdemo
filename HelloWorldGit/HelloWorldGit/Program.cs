70326569a36b941522454d5f348e4d672b5a3bbf
admin <admin@ADMIN-PC>
2017-04-24 10:39:56 -0700

    divide by zero issues introduced

--- HelloWorldGit/HelloWorldGit/Program.cs
+++ HelloWorldGit/HelloWorldGit/Program.cs
@@ -11,6 +11,10 @@ namespace HelloWorldGit
         static void Main(string[] args)
         {
             Console.WriteLine("hello world from git!");
+            int z = 0;
+            int y = 5;
+            int x = y / z;
+
         }
     }
 }
